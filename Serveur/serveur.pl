b491f393e8553b7e680faa107e74d1b705873ff8
Elise Carbonneau-Leclerc <elise654@hotmail.com>
2015-11-18 17:09:28 -0500

    Authentification du nom d'utilisateur avec le contenu du dossier du serveur

--- Serveur/serveur.pl
+++ Serveur/serveur.pl
@@ -1,114 +1,219 @@
 #!/usr/bin/env perl
 
+
+
 #Ajout des librairies
+
 use IO::Socket;
+
 use Digest::MD5 qw(md5_hex);
+
 use MIME::Lite;
+use File::Basename qw();
+
+
 
 #Declaration des variable
+
 my $protocole = "tcp";
-my $port = 2554;
+
+my $port = 2558;
+
 my $input = "";
+
 my $configfilepath = "/config/config.txt";
-my $userconfigfilepath = "/user/";
+
+#my $userconfigfilepath = "/user/";
+
 my $protocole = "tcp";
+
 my $printmenu = "Menu\n
+
 				1. Envoi de courriels\n
+
 				2. Consultation de courriels\n
+
 				3. Statistiques\n
+
 				4. Mode administrateur\n
+
 				5. Quitter\n";
 
 
 
+
+
+
+
+
 print "Avant main\n";
+
 &main;
 
 
 
+
+
+
+
 sub checkuservalidity
+
 {
 	print "Begin checkuservalidity\n";
+
+	my ($name, $path, $suffix) = File::Basename::fileparse($0);
+
 	my $username = $_[0];
+	print "Username recu $username\n";
+
 	my $cipheredpassword = $_[1];
-	open(my $fh, '<:encoding(UTF-8)', "$userconfigfilepath$username/config.txt")
-  		or die return false;
+	print "Password recu $cipheredpassword\n";
+	my $configfile = '/config.txt';
+	$path = "$path$username$configfile";
+	print "Path $path\n";
+	opendir (my $dh, $directory);
+
+	open(my $fh, '<:encoding(UTF-8)', $path)
+
+  		or die return 0;
+
+
 
 	my $firstline = <$fh>;
+	close $fh;
+	print "$firstline\n";
+
 	print "End checkuservalidity\n";
-	$firstline eq cipheredpassword;
+
+	#$firstline eq cipheredpassword;
+	return 1;
+
 }
+
+
+
+
 
 
 
 sub stringcontains
+
 {
+
 	print "Begin stringcontains\n";
+
 	my $mystring = $_[0];
+
 	my $searchedtext = $_[1];
 
+
+
 	print "End stringcontains\n";
+
 	$mystring =~ /$searchedtext/;
+
 }
+
+
 
 sub askclientidentification
+
 {
+
 	print "Begin askclientidentification\n";
+
 	my $clientconnection = $_[0];
+
 	my $username = "";
+
 	my $cipheredpassword = "";
+
 	my $successfulidentification;
+
 	
+
 	print $clientconnection "Veuillez vous identifier\n";
+
 	print $clientconnection "Nom d'utilisateur: \n";
+
 	chomp($username = <$clientconnection>);
+
 	print $clientconnection "Mot de passe: \n";
+
 	chomp($cipheredpassword = <$clientconnection>);
 
+
+
 	print "End askclientidentification\n";
-	&checkuservalidity($username, $cipheredpassword);
+
+	return &checkuservalidity($username, $cipheredpassword);
+
 }
+
+
 
 sub startserveur
+
 {
+
 	print "Begin startserveur\n";
+
 	my $port = $_[0];
+
 	$serveur = IO::Socket::INET->new( Proto => $protocole,
+
 	LocalPort => $port,
+
 	Listen => SOMAXCONN,
+
 	Reuse => 1)
+
 	or die "Impossible de se connecter sur le port $port en localhost";
+
 	print "End startserveur\n";
+
 	$serveur;
+
 }
 
+
+
 sub main
+
 {
+
 	print "Begin main\n";
+
   	my $server = &startserveur($port);
-#	while (my $connection = $serveur->accept())
-#	{
-#		print $connection "TP4 - Serveur de courriels\n";
-#		while($input ne "quit\r\n")
-#		{
-#			
-#
-#		}
-#		close($connection);
-#	}
-#
+
   	while (my $connection = $serveur->accept())
+
 	{
+
 		my $desirequitter = false;
+
 		if (&askclientidentification($connection))
+
 		{
+			print $connection "Authentification reussie\n";
+
 			while ($desirequitter eq false)
+
 			{
+
 				print $connection $printmenu;
+
 				chomp($input = <$connection>);
-				#print $input;
-				#print $connection "Merci ~serveur\n";
+
 			}
+
 		}
+		else
+		{
+			print $connection "Authentification echouee\n";
+			close($connection);
+		}
+
 	}
+
 }
