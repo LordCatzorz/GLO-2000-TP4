1e314b85aba0fb7f7cd02007ab2233dd4746fa8a
Lord Catzorz <LordCatzorz@users.noreply.github.com>
2015-11-19 16:28:41 -0500

    Envoie de courriel. Ajoute dans les dossiers si c'est un @reseauglo.ca

--- Serveur/serveur.pl
+++ Serveur/serveur.pl
@@ -10,6 +10,7 @@ use IO::Socket;
 
 use MIME::Lite;
 use File::Basename qw();
+use File::Path qw(make_path);
 
 
 
@@ -23,45 +24,25 @@ my $input = "";
 
 my $configfilepath = "/config/config.txt";
 
+my $adresseApplication = "reseauglo.ca";
 #my $userconfigfilepath = "/user/";
 
 my $protocole = "tcp";
 
 my $printmenu = "Menu\n
-
-				1. Envoi de courriels\n
-
-				2. Consultation de courriels\n
-
-				3. Statistiques\n
-
-				4. Mode administrateur\n
-
-				5. Quitter\n";
+1. Envoi de courriels\n
+2. Consultation de courriels\n
+3. Statistiques\n
+4. Mode administrateur\n
+5. Quitter\n";
 
 my $username = "";
 
-
-
-
-
-
-
-print "Avant main\n";
-
 &main;
-
-
-
-
-
-
 
 sub checkuservalidity
 
 {
-	print "Begin checkuservalidity\n";
-
 	my ($name, $path, $suffix) = File::Basename::fileparse($0);
 
 	my $username = $_[0];
@@ -75,15 +56,10 @@ sub checkuservalidity
 	opendir (my $dh, $directory);
 
 	open(my $fh, '<:encoding(UTF-8)', $path)
-
   		or die return 0;
-
-
 
 	my $firstline = <$fh>;
 	close $fh;
-
-	print "End checkuservalidity\n";
 
 	if ($firstline eq uc($cipheredpassword))
 	{
@@ -93,25 +69,13 @@ sub checkuservalidity
 
 }
 
-
-
-
-
-
-
 sub stringcontains
 
 {
 
-	print "Begin stringcontains\n";
-
 	my $mystring = $_[0];
 
 	my $searchedtext = $_[1];
-
-
-
-	print "End stringcontains\n";
 
 	$mystring =~ /$searchedtext/;
 
@@ -122,9 +86,6 @@ sub stringcontains
 sub askclientidentification
 
 {
-
-	print "Begin askclientidentification\n";
-
 	my $clientconnection = $_[0];
 
 	
@@ -139,22 +100,26 @@ sub askclientidentification
 	$clientconnection->send("Mot de passe:");
 	$clientconnection->recv($cipheredpassword, 1024);
 
-	my $successfulidentification = &checkuservalidity($username, $cipheredpassword);;
-
-	print "End askclientidentification ($successfulidentification)\n";
+	my $successfulidentification = &checkuservalidity($username, $cipheredpassword);
 
 	return $successfulidentification;
-
 }
 
+sub userexist
+{
+	my $searcheduser = $_[0];
 
+	print "User exist : ./$searcheduser\n";
+	if (-d "./$searcheduser")
+	{
+		return 1
+	}
+	return 0;
+}
 
 sub startserveur
 
 {
-
-	print "Begin startserveur\n";
-
 	my $port = $_[0];
 
 	$serveur = IO::Socket::INET->new( Proto => $protocole,
@@ -167,19 +132,88 @@ sub startserveur
 
 	or die "Impossible de se connecter sur le port $port en localhost";
 
-	print "End startserveur\n";
-
 	$serveur;
 
 }
 
+sub creerfichiermessage
+{
+	chomp(my $destadr = $_[0]);
+	chomp(my $ccadr = $_[1]);
+	chomp(my $sujet = $_[2]);
+	chomp(my $corps = $_[3]);
+
+	(my $destadruser, my $destadrdomain) = split ('@',$destadr);
+	(my $ccadruser, my $ccadrdomain) = split ('@',$ccadr);
+
+	($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+
+	my $contenuFichier = "Date et temps : $year-$mon-$mday $hour:$min:$sec\n
+De: $username$adresseApplication\n
+A: $destadr\n
+CC: $ccadr\n
+Sujet: $sujet\n
+Corps: \n
+$corps";
+	
+
+	my $filename = "$year$mon$mday-$hour$min$sec.txt";
+
+	if ($destadrdomain eq $adresseApplication)
+	{
+		my $destuser = $destadruser;
+		if (&userexist($destuser) eq 0)
+		{
+			$destuser = "DESTERREUR";
+		}
+		my $path = "./$destuser/recu/dest/";
+		print "Creation de fichier dans $path$filename\n";
+		eval{make_path($path)};
+		my $file = "$path$filename";
+		open FILE, '>'.$file;
+		print FILE $contenuFichier;
+		close FILE;
+	}
+	else
+	{
+		print "SMTP : Dest:$destadr\n";
+		#smtp
+	}
+
+	if ($ccadrdomain eq $adresseApplication)
+	{
+		my $ccuser = $ccadruser;
+		if (&userexist($dccuser) eq 0)
+		{
+			$ccuser = "DESTERREUR";
+		}
+		my $path = "./$ccuser/recu/cc/";
+		print "Creation de fichier dans $path$filename\n";
+		eval{make_path($path)};
+		my $file = "$path$filename";
+		open FILE, '>'.$file;
+		print FILE $contenuFichier;
+		close FILE;
+	}
+	else
+	{
+		print "SMTP : CC:$ccadr\n";
+		#smtp
+	}
+
+	my $path = "./$username/envoye/";
+	print "Creation de fichier dans $path$filename\n";
+	eval{make_path($path)};
+	my $file = "$path$filename";
+	open FILE, '>'.$file;
+	print FILE $contenuFichier;
+	close FILE;
+}
 
 
 sub main
 
 {
-
-	print "Begin main\n";
 
   	my $server = &startserveur($port);
 
@@ -200,6 +234,7 @@ sub main
 			#print $connection $printmenu;
 			while (1)
 			{
+				$connection->flush();
 				$connection->send($printmenu);
 				print "En attente du choix de menu\n";
 				$connection->recv($input, 1024);
@@ -216,15 +251,17 @@ sub main
 					$connection->recv($ccAdr, 1024);
 			
 					$connection->send("Quel est le sujet:");
-					my $subject = "";
-					$connection->recv($subject, 1024);
+					my $sujet = "";
+					$connection->recv($sujet, 1024);
 			
 					$connection->send("Quel est le corps du message:");
-					my $body = "";
-					$connection->recv($body, 1048576);
+					my $corps = "";
+					$connection->recv($corps, 1048576);
 	
-	
-	
+					
+					&creerfichiermessage($destAdr, $ccAdr, $sujet, $corps);
+					
+					$connection->send("Message envoyé");
 	
 				}
 				elsif ($input == "2")
