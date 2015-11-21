c4884b1b14e37e25be419c1213663b8c7051e994
Lord Catzorz <LordCatzorz@users.noreply.github.com>
2015-11-21 14:57:51 -0500

    AJout de fichier test

---
+++ c.pl
@@ -0,0 +1,29 @@
+use IO::Socket;
+use Digest::MD5 qw(md5_hex);
+
+my $username = "";
+my $password = "";
+
+my $server = IO::Socket::INET->new( Proto => "tcp",
+										PeerAddr => "localhost",
+										PeerPort => 2559);
+
+$server->recv(my $firstServerMessage, 1024);
+print "$firstServerMessage\n";
+$server->recv(my $serverAskUsernameMessage, 1024);
+while($username eq "")
+{
+	print "$serverAskUsernameMessage\n";
+	chomp($username = <STDIN>); 
+}
+$server->send($username);
+$server->recv(my $serverAskPasswordMessage, 1024);
+while($password eq "")
+{
+	print "$serverAskPasswordMessage\n";
+	chomp($password = <STDIN>);
+}
+my $hashedPassword = md5_hex($password);
+$server->send($hashedPassword);
+$server->recv(my $lastServerMessage, 1024);
+print $lastServerMessage;
---
+++ s.pl
@@ -0,0 +1,20 @@
+use IO::Socket;
+
+my $socket= IO::Socket::INET->new( Proto => "tcp",
+								   LocalPort => 2559,
+								   Listen => SOMAXCONN,
+								   Reuse => 1);
+
+while(1)
+{
+	print "Waiting for a client\n";
+	my $client = $socket->accept();
+	$client->send("Hello, please connect yourself");
+	$client->send("Username:");
+	$client->recv(my $username, 1024);
+	$client->send("Password:");
+	$client->recv(my $cipheredpassword, 1024);
+	$client->send("Thank you, Goodbye.");
+	$client->flush;
+	$client->close();
+}
